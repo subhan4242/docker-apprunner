31ba5047b89677f42364639455de918858df6a50
Subhan MURTAZA <sumu002@student.kristiania.no>
2025-09-26 16:31:04 +0200

    Add files via upload

---
+++ apprunner/src/main/java/com/example/apprunner/ApprunnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.apprunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApprunnerApplication.class, args);
+	}
+
+}
